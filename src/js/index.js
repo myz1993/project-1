dc0b7ab94bff5e3443738a5a0af9c0e9682c8b27
Leo Liu <leo.liu@vudoo.com>
2020-09-05 22:40:44 +1000

    feature: finsh home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
-xx.onclick = function () {
-  yy.classList.toggle('active')
-}
+'use strict';
+
+let menuButton;
+let sidebarMenu;
+
+const init = () => {
+  menuButton = document.querySelector('svg#menuButton');
+  sidebarMenu = document.querySelector('div#sidebarMenu');
+};
+
+const handleMenuButtonClick = () => {
+  sidebarMenu.classList.toggle('active');
+};
+
+const handleWindowResize = () => {
+  sidebarMenu.classList.remove('active');
+};
+
+const addListeners = () => {
+  menuButton.onclick = handleMenuButtonClick;
+  window.onresize = handleWindowResize;
+};
+
+window.onload = () => {
+  //init
+  init();
+  //add liseners
+  addListeners();
+};
