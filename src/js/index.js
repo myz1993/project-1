406aa6e011340fa2a2f0a28726924dbcd59002d0
Leo Liu <leo.liu@vudoo.com>
2020-09-06 16:49:49 +1000

    feature: resume page finish main section

--- src/js/index.js
+++ src/js/index.js
@@ -27,3 +27,63 @@ window.onload = () => {
   //add liseners
   addListeners();
 };
+
+const cardSection = document.querySelector('div.cardSection');
+
+let cardData = [
+  {
+    title: 'Melbourne University',
+    companyYear: '2020',
+    companyName: 'Apple Inc',
+    Description: 'Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.',
+  },
+
+  {
+    title: 'Melbourne University',
+    companyYear: '2020',
+    companyName: 'Apple Inc',
+    Description: 'Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.',
+  },
+
+  {
+    title: 'Melbourne University',
+    companyYear: '2020',
+    companyName: 'Apple Inc',
+    Description: 'Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.',
+  },
+];
+
+const createCard = (currentData) => {
+  const divCard = document.createElement('div');
+  divCard.classList.add('card');
+
+  const divCardTitle = document.createElement('div');
+  divCardTitle.classList.add('card_title');
+  divCardTitle.innerText = currentData.title;
+  console.log(divCardTitle);
+
+  const divCardCompany = document.createElement('div');
+  divCardCompany.classList.add('card_company');
+
+  const spanCompanyYear = document.createElement('span');
+  spanCompanyYear.classList.add('companyYear');
+  spanCompanyYear.innerText = currentData.companyYear;
+
+  const spanCompanyName = document.createElement('span');
+  spanCompanyName.classList.add('companyName');
+  spanCompanyName.innerText = currentData.companyName;
+
+  const p = document.createElement('p');
+  p.innerText = currentData.Description;
+
+  divCardCompany.appendChild(spanCompanyYear);
+  divCardCompany.appendChild(spanCompanyName);
+
+  divCard.appendChild(divCardTitle);
+  divCard.appendChild(divCardCompany);
+  divCard.appendChild(p);
+
+  cardSection.appendChild(divCard);
+};
+
+cardData.forEach(createCard);
